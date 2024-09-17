b4bdcd52408f0fbe52b8cdacaf64ce4e5145301a
Senokotvsky <magmalatte@outlook.com>
2024-09-17 12:07:06 +0800

    some config

--- src/config.ts
+++ src/config.ts
@@ -25,12 +25,12 @@ export const siteConfig: SiteConfig = {
       url: ''                // (Optional) URL link to the original artwork or artist's page
     }
   },
-  favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+  favicon: [     
+     {
+       src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+       //theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+       //sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
   ]
 }
 
