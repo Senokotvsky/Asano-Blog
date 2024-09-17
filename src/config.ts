cb1e671dfe948d806b61e8353daf66fccec3d751
Senokotvsky <magmalatte@outlook.com>
2024-09-17 11:49:38 +0800

    rollback

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+import { flattenDiagnosticMessageText } from 'typescript'
 import type {
   LicenseConfig,
   NavBarConfig,
@@ -12,12 +13,12 @@ export const siteConfig: SiteConfig = {
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 80,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: true,     // Hide the theme color picker for visitors
+    fixed: false,     // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
     src: 'assets/images/Banner_new!.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center', // Equivalent to object-position, defaults center
+    position: 'top', // Equivalent to object-position, defaults center
     credit: {
       enable: false,         // Display the credit text of the banner image
       text: '',              // Credit text to be displayed
@@ -45,7 +46,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/Asano.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: '谢星霖',
-  bio: 'aka “Asano”，一个路过的在校生。',
+  bio: 'aka “Asano”，努力创造一些有意思的事物。',
   links: [
     {
       name: 'Mail',
@@ -69,6 +70,6 @@ export const profileConfig: ProfileConfig = {
 
 export const licenseConfig: LicenseConfig = {
   enable: true,
-  name: 'CC BY-NC-ND 4.0',
-  url: 'https://creativecommons.org/licenses/by-nc-nd/4.0/deed.zh-hans',
+  name: 'CC BY-NC-SA 4.0',
+  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh-hans',
 }
